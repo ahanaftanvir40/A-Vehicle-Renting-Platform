e9b7f92223d378705bfc89d609835d1f0ec343b6
Ahanaf Tanvir <ahanaf.tanvir40@gmail.com>
2024-12-31 06:08:17 +0600

    up

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -165,27 +165,6 @@ function Home() {
           </p>
         </div>
       </div>
-      {/* Fixed Chat Button */}
-      <Link
-        to="/chatai"
-        className="fixed bottom-8 right-8 bg-blue-600 text-white rounded-full p-4 shadow-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300"
-      >
-        Chat with AI
-        <svg
-          className="w-5 h-5 inline ml-2"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth="2"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M13 17h8m0 0v8m0-8l-8-8m8 8L9 3m0 0v8m0-8H1"
-          />
-        </svg>
-      </Link>
     </div>
   );
 }
